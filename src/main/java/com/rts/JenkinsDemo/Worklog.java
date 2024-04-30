9ab2b0558d445239a07996d08396315a685786eb
karpagam <karpagambs26@gmail.com>
2024-04-30 10:10:08 +0530

    first commit

---
+++ src/main/java/com/rts/JenkinsDemo/Worklog.java
@@ -0,0 +1,13 @@
+package com.rts.JenkinsDemo;
+
+public class Worklog {
+
+	public String addToLog(String name) {
+		return "Added Log For" + name;
+	}
+
+	public String addLogins(String loginname) {
+		return "Added Login Log For" + loginname;
+	}
+
+}
